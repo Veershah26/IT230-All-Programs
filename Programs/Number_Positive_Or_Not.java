ab761625dcb5c69defd4f4cdcf480720f61ad857
Veershah26 <shah.veer18@gmail.com>
2021-02-09 23:06:02 +0530

    Added new files

---
+++ Programs/Number_Positive_Or_Not.java
@@ -0,0 +1,14 @@
+class number_positive_or_not {
+    public static void main(String[] args) {
+        int Number=45672;
+        if (Number > 0) {
+                System.out.println(Number + " Is A Positive Number");
+            }
+        else if (Number < 0) {
+                System.out.println(Number + " Is A Negative Number");
+            }
+        else {
+                System.out.println(Number + " Is Neither Positive Nor Negative");
+            }
+    }
+}
